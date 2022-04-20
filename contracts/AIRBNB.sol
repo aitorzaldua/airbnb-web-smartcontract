bbb0e5fdb055c73b5c98c93f3b5e4ac61e03d3b6
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-04-20 19:53:25 +0200

    200422 contract init

---
+++ contracts/AIRBNB.sol
@@ -0,0 +1,91 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract airbnb {
+
+    address public owner;
+    uint256 private counter;
+
+    constructor() {
+        counter = 0;
+        owner = msg.sender;
+    }
+
+    struct rentalInfo {
+        string name;
+        string city;
+        string lat;
+        string long;
+        string unoDescription;
+        string dosDescription;
+        string imgUrl;
+        uint256 maxGuests;
+        uint256 pricePerDay;
+        string[] datesBooked;
+        uint256 id;
+        address renter;
+    }
+
+    event rentalCreated (
+        string name,
+        string city,
+        string lat,
+        string long,
+        string unoDescription,
+        string dosDescription,
+        string imgUrl,
+        uint256 maxGuests,
+        uint256 pricePerDay,
+        string[] datesBooked,
+        uint256 id,
+        address renter
+    );
+
+    event newDatesBooked (
+        string[] datesBooked,
+        uint256 id,
+        address booker,
+        string city,
+        string imgUrl
+    );
+
+    mapping(uint256 => rentalInfo) rentals;
+    uint256[] public rentalIds;
+
+    function addRentals(
+        string memory name,
+        string memory city,
+        string memory lat,
+        string memory long,
+        string memory unoDescription,
+        string memory dosDescription,
+        string memory imgUrl,
+        uint256 maxGuests,
+        uint256 pricePerDay,
+        string[] memory datesBooked
+    ) public {
+
+        require(msg.sender == owner, "Only the owner of the smart contract can put up rentals");
+
+        rentalInfo storage newRental = rentals[counter];
+        newRental.name = name;
+        newRental.city = city;
+        newRental.lat = lat;
+        newRental.long = long;
+        newRental.unoDescription = unoDescription;
+        newRental.dosDescription = dosDescription;
+        newRental.imgUrl = imgUrl;
+        newRental.maxGuests = maxGuests;
+        newRental.pricePerDay = pricePerDay;
+        newRental.datesBooked = datesBooked;
+        newRental.id = counter;
+        newRental.renter = owner;
+        rentalIds.push(counter);
+
+
+
+
+    }
+
+
+}
